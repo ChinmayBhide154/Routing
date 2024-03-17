58b9574d169028568cb07a0cb39cffae2fafd33b
Chinmay Bhide <chinmay.bhide154@gmail.com>
2024-03-17 15:47:23 -0700

    setting up and preparing for main merge

--- src/distancevector.py
+++ src/distancevector.py
@@ -1,7 +1,18 @@
 from network import Network
+import sys
 
-graph = []
+def dv():
+    return 1
 
-network = Network(graph)
-network.build_network("C:/Git Repositories/Routing/src/topology.txt")
-print(network.graph)
+
+if __name__ == '__main__':
+    if len(sys.argv) != 4:
+        print("wrong number of arguments provided to program")
+        sys.exit(1)
+    topologyFile = sys.argv[1]
+    messageFile = sys.argv[2]
+    changesFile = sys.argv[3]
+
+    network = Network(topologyFile, messageFile, changesFile)
+    network.build_network()
+    #print(network.graph)
--- src/network.py
+++ src/network.py
@@ -1,11 +1,25 @@
 class Network:
-    def __init__(self, graph):
-        self.graph = graph
+    def __init__(self, topologyFile, messageFile, changesFile):
+        self.topologyFile = topologyFile
+        self.messageFile = messageFile
+        self.changesFile = changesFile
+        self.graph = []
+        self.messages = []
 
-    def build_network(self, file):
-        with open(file, 'r') as topology:
+    def build_network(self):
+        with open(self.topologyFile, 'r') as topology:
             for line in topology:
                 node1, node2, cost = map(int, line.split())
                 self.graph.append([node1, node2, cost])
+
+    def parseMessages(self):
+        with open(self.messageFile, 'r') as messages:
+            for line in messages:
+                node1, node2, message_text = messages.readline().strip().split(maxsplit=2)
+                self.messages.append([node1, node2, message_text])
+
+    
+
+    
 
                 
