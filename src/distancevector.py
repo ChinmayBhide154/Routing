38daddd3ff658b0e1e1d7c2dab57941b4c7ab4c8
Chinmay Bhide <chinmay.bhide154@gmail.com>
2024-03-17 15:58:08 -0700

    adding bellman ford function

--- src/distancevector.py
+++ src/distancevector.py
@@ -1,8 +1,29 @@
 from network import Network
 import sys
 
-def dv():
-    return 1
+INFINITY = 99999
+
+def bellman_ford(dst, routers, links):
+    distance = {}
+    nexthop = {}
+    
+    for r in routers:
+        distance[r] = INFINITY
+        nexthop[r] = None
+
+    distance[dst] = 0
+
+    for _ in range(len(routers) - 1):
+        for (r1, r2, dist) in links:
+            if distance[r1] + dist < distance[r2]:
+                distance[r2] = distance[r1] + dist
+                nexthop[r2] = r1
+
+    return distance, nexthop
+
+
+
+
 
 
 if __name__ == '__main__':
