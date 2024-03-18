cc2eced0a9b577f4472db5be4bc1e9c95fca286b
Chinmay Bhide <chinmay.bhide154@gmail.com>
2024-03-18 12:10:47 -0700

    almost done, testing needed

--- src/distancevector.py
+++ src/distancevector.py
@@ -22,18 +22,15 @@ def bellman_ford(dst, routers, links):
     return distance, nexthop
 
 
-
-
-
-
 if __name__ == '__main__':
     if len(sys.argv) != 4:
         print("wrong number of arguments provided to program")
         sys.exit(1)
+
     topologyFile = sys.argv[1]
     messageFile = sys.argv[2]
     changesFile = sys.argv[3]
 
     network = Network(topologyFile, messageFile, changesFile)
     network.build_network()
-    #print(network.graph)
+    print(network.graph)
--- src/network.py
+++ src/network.py
@@ -5,6 +5,8 @@ class Network:
         self.changesFile = changesFile
         self.graph = []
         self.messages = []
+        self.routing_tables = {}
+        
 
     def build_network(self):
         with open(self.topologyFile, 'r') as topology:
@@ -17,6 +19,47 @@ class Network:
             for line in messages:
                 node1, node2, message_text = messages.readline().strip().split(maxsplit=2)
                 self.messages.append([node1, node2, message_text])
+
+    def receiveDV(self, router_id):
+        for source, dest, cost in self.graph:
+            if source != router_id:
+                continue
+
+            if dest not in self.routing_tables:
+                self.routing_tables[dest] = {}
+
+            (self.routing_tables[dest])[source][dest] = cost
+
+    def computeDV(self, router_id):
+        distance_vector = {router_id: 0}
+        # Update the distance vector from self.graph
+        for source, dest, cost in self.graph:
+            if source != router_id:
+                continue
+            if dest not in self.routing_tables:
+                self.routing_tables[dest] = {}
+            self.routing_tables[dest][source] = cost
+
+        # Iterate over the routing tables to compute the distance vector
+        for dest, routes in self.routing_tables.items():
+            if dest == router_id:
+                continue
+            min_cost = float('inf')
+            for source, cost in routes.items():
+                if source == router_id:
+                    continue
+                if source in distance_vector:
+                    min_cost = min(min_cost, distance_vector[source] + cost)
+            distance_vector[dest] = min_cost
+
+        return distance_vector
+
+
+
+
+
+
+    
 
     
 
