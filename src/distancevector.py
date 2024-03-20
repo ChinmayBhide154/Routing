3ea10abead56100886104e5744cd5be8848a5222
Chinmay Bhide <chinmay.bhide154@gmail.com>
2024-03-20 00:39:23 -0700

    distance vector starting to work

--- src/distancevector.py
+++ src/distancevector.py
@@ -1,15 +1,27 @@
 from network import Network
 import sys
 
-INFINITY = 99999
+def parse_topology(topology):
+    """
+    Parses the topology file and returns a graph represented as a dictionary of dictionaries.
+    """
+    graph = {}
+    for r1, r2, dist in topology:
+        if r1 not in graph:
+            graph[r1] = {}
+        graph[r1][r2] = dist
+        if r2 not in graph:
+            graph[r2] = {}
+        graph[r2][r1] = dist  # Assuming the graph is undirected
+    return graph
 
 def bellman_ford(dst, routers, links):
-    distance = {}
-    nexthop = {}
-    
-    for r in routers:
-        distance[r] = INFINITY
-        nexthop[r] = None
+    """
+    Runs the Bellman-Ford algorithm for a given destination router.
+    """
+    INFINITY = float('inf')
+    distance = {r: INFINITY for r in routers}
+    nexthop = {r: None for r in routers}
 
     distance[dst] = 0
 
@@ -21,16 +33,45 @@ def bellman_ford(dst, routers, links):
 
     return distance, nexthop
 
+def distance_vector_routing(topology):
+    """
+    Simulates the Distance Vector Routing Protocol using the Bellman-Ford algorithm.
+    """
+    graph = parse_topology(topology)
+    routers = list(graph.keys())
+    links = [(r1, r2, graph[r1][r2]) for r1 in graph for r2 in graph[r1]]
+
+    distance_vectors = {}
+    next_hops = {}
+
+    for router in routers:
+        distance_vectors[router], next_hops[router] = bellman_ford(router, routers, links)
+
+    return distance_vectors, next_hops
+
 
 if __name__ == '__main__':
-    if len(sys.argv) != 4:
-        print("wrong number of arguments provided to program")
-        sys.exit(1)
+    #if len(sys.argv) != 4:
+     #   print("wrong number of arguments provided to program")
+      #  sys.exit(1)
 
-    topologyFile = sys.argv[1]
-    messageFile = sys.argv[2]
-    changesFile = sys.argv[3]
+    #topologyFile = sys.argv[1]
+    #messageFile = sys.argv[2]
+    #changesFile = sys.argv[3]
+    topology = [
+        (1, 2, 8),
+        (2, 3, 3),
+        (2, 5, 4),
+        (4, 1, 1),
+        (4, 5, 1)
+    ]
 
-    network = Network(topologyFile, messageFile, changesFile)
-    network.build_network()
-    print(network.graph)
+    # Running the simulation
+    final_distance_vectors, final_next_hops = distance_vector_routing(topology)
+    print("Distance Vectors:")
+    print(final_distance_vectors)
+    print("Next Hops:")
+    print(final_next_hops)
+    #topologyFile = "C:\\Git Repositories\\Routing\\src\\topology.txt"
+    #messageFile = "C:\\Git Repositories\\Routing\\src\\message.txt"
+    #changesFile = "C:\\Git Repositories\\Routing\\src\\changes.txt"
--- src/network.py
+++ src/network.py
@@ -6,48 +6,57 @@ class Network:
         self.graph = []
         self.messages = []
         self.routing_tables = {}
-        
+    '''    
+    def initRoutingTables(self):
+        for source, dest, cost in self.graph:
+            if source not in self.routing_tables:
+                self.routing_tables[source] = {}
+            if dest not in self.routing_tables:
+                self.routing_tables[dest] = {}
+            self.routing_tables[source][dest] = cost
+            self.routing_tables[dest][source] = cost
 
     def build_network(self):
         with open(self.topologyFile, 'r') as topology:
             for line in topology:
                 node1, node2, cost = map(int, line.split())
-                self.graph.append([node1, node2, cost])
+                self.graph.append((node1, node2, cost))
 
     def parseMessages(self):
         with open(self.messageFile, 'r') as messages:
             for line in messages:
                 node1, node2, message_text = messages.readline().strip().split(maxsplit=2)
-                self.messages.append([node1, node2, message_text])
+                self.messages.append((node1, node2, message_text))
 
-    def receiveDV(self, router_id):
-        for source, dest, cost in self.graph:
-            if source != router_id:
-                continue
-
-            if dest not in self.routing_tables:
-                self.routing_tables[dest] = {}
-
-            (self.routing_tables[dest])[source][dest] = cost
 
     def computeDV(self, router_id):
-        INFINITY = float('inf')
-        routers = set()
-        links = []
-        for source, dest, cost in self.graph:
-            routers.add(source)
-            routers.add(dest)
-            links.append((source, dest, cost))
+    # Use Bellman-Ford algorithm to update routing table based on neighbors' distance vectors
+    # Ensure router_id is in the routing tables
+        if router_id not in self.routing_tables:
+            print(f"Router ID {router_id} not found in the routing tables.")
+            return
 
-        distance, nexthop = self.bellman_ford(router_id, routers, links)
+        for source in self.routing_tables:
+            # Skip if the source is the same as the router_id
+            if source == router_id:
+                continue
 
-        distance_vector = {}
-        for dest in distance:
-            if dest != router_id:
-                distance_vector[dest] = distance[dest]
+            for dest in self.routing_tables[source]:
+                # Skip if the destination is the router itself
+                if dest == router_id:
+                    continue
 
-        return distance_vector
-    
+                # Compute the new cost if possible
+                source_to_dest_cost = self.routing_tables[source].get(dest, float('inf'))
+                router_to_source_cost = self.routing_tables[router_id].get(source, float('inf'))
+                existing_cost_to_dest = self.routing_tables[router_id].get(dest, float('inf'))
+
+                # Calculate new cost and update if it's cheaper
+                new_cost = router_to_source_cost + source_to_dest_cost
+                if new_cost < existing_cost_to_dest:
+                    self.routing_tables[router_id][dest] = new_cost
+                    print(f"Updated cost to {dest} through {source} to {new_cost}")
+    ''' 
     def bellman_ford(self, dst, routers, links):
         INFINITY = float('inf')
         distance = {}
@@ -59,13 +68,18 @@ class Network:
 
         distance[dst] = 0
 
-        for _ in range(len(routers) - 1):
-            for (r1, r2, dist) in links:
-                if distance[r1] + dist < distance[r2]:
-                    distance[r2] = distance[r1] + dist
-                    nexthop[r2] = r1
+        #for _ in range(len(routers) - 1):
+        for (r1, r2, dist) in links:
+            if distance[r1] + dist < distance[r2]:
+                distance[r2] = distance[r1] + dist
+                nexthop[r2] = r1
 
         return distance, nexthop
+    ''' 
+    def print_routing_tables(self):
+        for router, table in self.routing_tables.items():
+            print(f"Router {router}: {table}")
+    ''' 
 
 
 
